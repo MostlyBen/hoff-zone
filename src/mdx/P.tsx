02ea6eb75f3c3d31b7dc0b8462957de54b1760c0
MostlyBen <benthehoff@gmail.com>
2024-05-01 17:13:03 -0400

    Added hashtag styling to paragraph elements

---
+++ src/mdx/P.tsx
@@ -0,0 +1,38 @@
+import { JSX } from "react";
+
+const Tag = ( { children }: JSX.IntrinsicElements["p"] ) => {
+  return (
+    <span
+      className="tag"
+      style={{
+        borderRadius: '4px',
+        padding: '2px 4px',
+      }}
+    >
+      <span>{children}</span>
+    </span>
+  )
+}
+
+const P = ( { children }: JSX.IntrinsicElements["p"] ) => {
+
+  if (typeof children === 'string') {
+    const parts = children.split(/(#\S+)/g)
+    return (
+      <p>
+        {parts.map((part, index) => {
+          if (part.match(/#\S+/)) {
+            return <Tag key={index}>{part}</Tag>
+          }
+          return part
+        })}
+      </p>
+    )
+  }
+
+  return (
+    <p>{children}</p>
+  )
+}
+
+export default P;
--- src/mdx/index.ts
+++ src/mdx/index.ts
@@ -1,3 +1,4 @@
 export { default as A } from './A';
 export { default as H1 } from './H1';
 export { default as HR } from './HR';
+export { default as P } from './P';
