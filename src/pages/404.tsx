e69829bdfa3ebf8f9f9586cd60ef8ebec55957d6
MostlyBen <benthehoff@gmail.com>
2024-07-07 15:24:29 -0400

    Added 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,14 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import React from 'react';
 
 const NotFoundPage = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  React.useEffect(() => {
-    router.replace('/');
-  });
+  // React.useEffect(() => {
+  //   router.replace('/');
+  // });
 
-  return null;
+  return <div>The page you are looking for does not exist.</div>;
 };
 
 export default NotFoundPage;
