509fe0e0ec9f783fefc26190296111a85335d310
MostlyBen <benthehoff@gmail.com>
2024-07-07 15:30:56 -0400

    Added page for error 500

---
+++ src/pages/500.tsx
@@ -0,0 +1,14 @@
+// import { useRouter } from 'next/router';
+import React from 'react';
+
+const NotFoundPage = () => {
+  // const router = useRouter();
+
+  // React.useEffect(() => {
+  //   router.replace('/');
+  // });
+
+  return <div>Ooops, looks like there was a problem on our end!</div>;
+};
+
+export default NotFoundPage;
