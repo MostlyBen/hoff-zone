becec279e425a7fb52708dd42e426682ac78e279
MostlyBen <benthehoff@gmail.com>
2024-07-09 09:32:18 -0400

    Transpile next-mdx-remote for compatibility with next 14.2.4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "mdx"],
+  transpilePackages: ["next-mdx-remote"]
 };
 
 const withMDX = require("./mdx-loader")();
