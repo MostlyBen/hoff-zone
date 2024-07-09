f245d8fdbaf8829f8efa09fd8ecd86c2fa5ecfe5
MostlyBen <benthehoff@gmail.com>
2024-07-09 14:43:54 -0400

    Updated commandExists to not break build

--- utils/console/commandExists.ts
+++ utils/console/commandExists.ts
@@ -1,9 +1,9 @@
 import * as bin from '../bin';
 import * as hidden from '../hidden';
 
-const allCommands = {...bin, ...hidden}
 
 export const commandExists = (command: string) => {
+  const allCommands = {...bin, ...hidden}
   const commands = ['clear', ...Object.keys(allCommands)];
 
   return commands.indexOf(command.split(' ')[0]) !== -1;
