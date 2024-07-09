366aa300a8840ea916b394b11cbf90362c624377
MostlyBen <benthehoff@gmail.com>
2024-07-09 09:15:27 -0400

    Removed unnecessary next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  appDir: true,
   pageExtensions: ["ts", "tsx", "mdx"],
-  experimental: {
-    mdxRs: true,
-  }
 };
 
 const withMDX = require("./mdx-loader")();
