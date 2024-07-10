8c785646f913108a690d7031f1cf3c1236c77548
MostlyBen <benthehoff@gmail.com>
2024-07-10 14:41:31 -0400

    Changed language on help command

--- utils/bin/utils.ts
+++ utils/bin/utils.ts
@@ -4,7 +4,7 @@ import * as bin from './index';
 export const help = async (_args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
 
-  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
+  return `Available commands:\n${commands}\n\n[tab]\t auto-complete.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
 export const journal = (_args?: string[]) => {
