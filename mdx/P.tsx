7ad4ee8ecaa6d7b7877d8e45e5294a1db2214540
MostlyBen <benthehoff@gmail.com>
2024-07-10 18:13:44 -0400

    Audo-detect links in paragraphs

--- mdx/P.tsx
+++ mdx/P.tsx
@@ -1,6 +1,5 @@
 import { JSX } from "react";
-
-// TODO: audo-detect links
+import A from "./A";
 
 const Tag = ( { children }: JSX.IntrinsicElements["p"] ) => {
   return (
@@ -15,12 +14,23 @@ const Tag = ( { children }: JSX.IntrinsicElements["p"] ) => {
 const P = ( { children, style }: JSX.IntrinsicElements["p"] ) => {
 
   if (typeof children === 'string') {
-    const parts = children.split(/(#\S+)/g)
+    const parts = children
+                  .replaceAll(/https?:.*(?=\s|$)/g, (m) => {return `$$LINK$$${m}`})
+                  .split(/(#\S+)/g)
     return (
       <p style={style}>
         {parts.map((part, index) => {
           if (part.match(/#\S+/)) {
             return <Tag key={index}>{part}</Tag>
+          }
+          if (part.startsWith('$$LINK$$')) {
+            let link = part.split('$$LINK$$')[1]
+            let text = link;
+            // Truncate long links
+            if (text.length > 50) {
+              text = text.substring(0, 25) + '...' + text.substring(text.length - 25, link.length);
+            }
+            return <A href={link} target="_blank" key={Math.random()}>{text}</A>
           }
           return part
         })}
