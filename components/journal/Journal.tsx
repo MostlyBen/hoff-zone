fbcde4b942db37b74720ee33bad9a52328c15fa8
MostlyBen <benthehoff@gmail.com>
2024-07-10 17:35:13 -0400

    Added MDX preview of journal

--- components/journal/Journal.tsx
+++ components/journal/Journal.tsx
@@ -3,14 +3,50 @@
 // TODO: render markdown on un-focus
 // + auto-detect links
 
-import { useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import { MDXRemote } from 'next-mdx-remote';
 import { useTheme } from 'utils/providers/themeProvider';
-import config from '../../theme-config.json';
 import { useStoredState } from 'hooks';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
+import config from '../../theme-config.json';
+import * as MDXComponents from "../../mdx";
+
+interface PreviewProps {
+  content: string;
+  onEdit: () => void;
+}
 
 interface JournalProps {
   journalId: string;
   onClose: () => void;
+}
+
+const JournalPreview:React.FC<PreviewProps> = ({ content, onEdit }) => {
+  const [serializedContent, setSerializedContent]
+    = useState<MDXRemoteSerializeResult|null>(null);
+
+  useEffect(() => {
+    serialize(content).then(res => setSerializedContent(res));
+  }, [content])
+
+  return (
+    <div
+      className='h-full w-full px-2'
+      onClick={(e:any) => {
+        if (e.target.classList[0] !== 'todo-check') {
+          onEdit();
+        }
+      }}
+    >
+      {serializedContent &&
+        <MDXRemote
+          components={MDXComponents}
+          {...serializedContent}
+        />
+      }
+    </div>
+  )
 }
 
 const Journal:React.FC<JournalProps> = ({ journalId, onClose }) => {
@@ -20,6 +56,7 @@ const Journal:React.FC<JournalProps> = ({ journalId, onClose }) => {
   }, [journalId]);
 
   const [content, setContent] = useStoredState<string>(journalPath, '');
+  const [showPreview, setShowPreview] = useState<boolean>(false);
 
   const handleKeyDown = (e:React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.ctrlKey && e.key === "x") {
@@ -36,25 +73,37 @@ const Journal:React.FC<JournalProps> = ({ journalId, onClose }) => {
         borderWidth: config.border ? 2 : 0,
       }}
     >
-      <textarea
-        className="grow overflow-y-auto p-2"
-        placeholder='put notes here...'
-        style={{
-          background: 'transparent',
-          backdropFilter: 'brightness(0.8)',
-          color: theme.foreground,
-          outline: 'none',
-          border: 'none',
-          resize: 'none',
-          borderRadius: "0.25em"
-        }}
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        onKeyDown={handleKeyDown}
-        autoFocus={true}
-      />
 
-      <div>[ctrl + x]: close</div>
+      {showPreview
+        ? <JournalPreview content={content} onEdit={() => setShowPreview(false)} />
+        : <>
+            <textarea
+              className="grow overflow-y-auto p-2"
+              placeholder='put notes here...'
+              style={{
+                background: 'transparent',
+                backdropFilter: 'brightness(0.8)',
+                color: theme.foreground,
+                outline: 'none',
+                border: 'none',
+                resize: 'none',
+                borderRadius: "0.25em"
+              }}
+              value={content}
+              onChange={(e) => {
+                setContent(
+                  e.target.value
+                  .replaceAll('<', '(') // Caret brackets break `serialize`
+                  .replaceAll('>', ')')
+                )
+              }}
+              onKeyDown={handleKeyDown}
+              autoFocus={true}
+              onBlur={() => { if (content) setShowPreview(true) }}
+            />
+            <div>[ctrl + x]: close</div>
+          </>
+      }
 
     </div>
   )
