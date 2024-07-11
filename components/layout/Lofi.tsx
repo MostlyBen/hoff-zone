04d24e44be0f7d55fcf594ff1f300d1b0488e9cc
MostlyBen <benthehoff@gmail.com>
2024-07-11 14:48:58 -0400

    Added lofi options

--- components/layout/Lofi.tsx
+++ components/layout/Lofi.tsx
@@ -1,4 +1,18 @@
+const options = [
+  "https://www.youtube.com/embed/zuCRSwWssVk",
+  "https://www.youtube.com/embed/6P0HiR4xWI4",
+  "https://www.youtube.com/embed/zhDwjnYZiCo",
+  "https://www.youtube.com/embed/PLLRRXURicM",
+  "https://www.youtube.com/embed/sF80I-TQiW0",
+  "https://www.youtube.com/embed/dN6LK9-Nx3I",
+  "https://www.youtube.com/embed/eEZF9iIv5XM",
+  "https://www.youtube.com/embed/MtT5_PgLJlY",
+  "https://www.youtube.com/embed/ZmdEBzCqL6o",
+  "https://www.youtube.com/embed/u4DXqKV0Wtk",
+]
 const Lofi = ({ onClose }) => {
+  const choice:number = Math.floor( Math.random() * options.length )
+
   return (
     <div style={{
       background: "var(--background)",
@@ -21,14 +35,14 @@ const Lofi = ({ onClose }) => {
         lineHeight: "1",
       }}><span className="material-icons" style={{lineHeight: '1'}}>close</span></button>
 
-      <iframe
-        width="420"
-        height="236"
-        src="https://www.youtube.com/watch?v=zuCRSwWssVk"
-        title="LoFi"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-      />
+    <iframe
+      width="420"
+      height="236"
+      src={options[choice]}
+      title="Lofi Youtube Video"
+      referrerPolicy="strict-origin-when-cross-origin"
+    />
+
     </div>
   )
 }
