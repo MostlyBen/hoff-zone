2c31e26dc2502545537200b2d89be86fc40e4c97
MostlyBen <benthehoff@gmail.com>
2024-07-22 09:59:39 -0400

    Added comments

--- hooks/useStoredState.ts
+++ hooks/useStoredState.ts
@@ -9,6 +9,7 @@ type StoredState<T> = [T, Dispatch<SetStateAction<T>>];
 
 function useStoredState<T>(stateName: string, defaultValue?:T, pathSpecific:boolean = true): StoredState<T> {
   const [value, setValue] = useState(defaultValue ?? null);
+  // don't have window on pre-render (serverside) so, avoid that error
   const key = (pathSpecific && typeof window !== 'undefined')
               ? window.location.pathname + '-' + stateName
               : stateName
@@ -28,6 +29,7 @@ function useStoredState<T>(stateName: string, defaultValue?:T, pathSpecific:bool
 
   useEffect(() => {
     updateFromStorage(key);
+    // useEffect doesn't run on server-side renders, so this is okay
     window.addEventListener("storage", e => {
       if (e.key === key) {
         updateFromStorage(key)
@@ -43,6 +45,9 @@ function useStoredState<T>(stateName: string, defaultValue?:T, pathSpecific:bool
     }
   }, [key])
 
+  // This function will be a problem if it ever has a reason to run server-side
+  // Like, if it's updated outside of an event or useEffect
+  // But that probably won't happen
   const updateValue = (newValue: any) => {
     setValue(newValue);
     if (newValue !== null) {
