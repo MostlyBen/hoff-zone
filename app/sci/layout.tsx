bd54bba3e630e0fe485ca81ddbb241527a18b1ce
MostlyBen <benthehoff@gmail.com>
2024-07-22 09:42:33 -0400

    Added comment

--- app/sci/layout.tsx
+++ app/sci/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import Decorator from "./decorator";
 
+// Overwritten in decorator in useEffect hook
+// Pretty much just here in case I later add a /sci page
 export const metadata: Metadata = { title: "/sci" };
 
 const SciLayout = ({ children }) => {
