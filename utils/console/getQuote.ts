b66be56138c812646a0735c4a57511642c7e7904
MostlyBen <benthehoff@gmail.com>
2024-09-23 11:05:03 -0400

    Switch cowquote request http

--- utils/console/getQuote.ts
+++ utils/console/getQuote.ts
@@ -1,5 +1,5 @@
 export const getQuote = async () => {
-  const res = await fetch('https://api.quotable.io/random');
+  const res = await fetch('http://api.quotable.io/random');
   const data = await res.json();
 
   return {
